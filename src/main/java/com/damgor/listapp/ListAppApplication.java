4139e72ab8d025ce2fcf216dafc87e752f13e50e
damgor <damian.gorka94@gmail.com>
2020-08-14 07:16:04 +0200

    Initial commit

---
+++ src/main/java/com/damgor/listapp/ListAppApplication.java
@@ -0,0 +1,13 @@
+package com.damgor.listapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListAppApplication.class, args);
+	}
+
+}
