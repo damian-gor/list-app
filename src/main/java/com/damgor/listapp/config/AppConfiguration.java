3c824424160e9649ca73ec799ae01f4e5372ae62
damgor <damian.gorka94@gmail.com>
2020-10-08 20:00:15 +0200

    git commit -m "repaired CORS problem"

--- src/main/java/com/damgor/listapp/config/AppConfiguration.java
+++ src/main/java/com/damgor/listapp/config/AppConfiguration.java
@@ -17,7 +17,7 @@ public class AppConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200","https://list-app-frontend.web.app/")
+                        .allowedOrigins("http://localhost:4200","https://list-app-frontend.web.app")
                         .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
             }
         };
